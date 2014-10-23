1610e791e9948bd9d50391e31d239d89c77d0f11
KlausRaynor <klaus.a.holder@gmail.com>
2014-10-22 21:28:06 -0400

    added a number of new things.  All user faces, and set up basic views and routing tests to see if they deploy correctly on the server.  grabbed and implemented a lorem ipsum package as well.

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,47 @@
 |
 */
 
+#Homepage
 Route::get('/', function()
 {
-	return View::make('hello');
+
+/*
+	$generator = new Badcow\LoremIpsum\Generator();
+	$paragraphs = $generator->getParagraphs(5);
+	
+	echo implode('<p>', $paragraphs);
+
+*/
+	return View::make('index');
+});
+
+Route::get('/users/{number?}', function($number) {
+
+	return 'List specified # of users';
+});
+
+#Display the form for a new user
+Route::get('/add', function() {
+
+});
+
+#Process the form for a new user
+Route::post('/add', function() {
+
+});
+
+#Display the form to edit a user
+Route::get('/edit/{name}', function() {
+
+});
+
+#Process the form to edit a user
+Route::post('/edit/', function() {
+
+});
+
+#practice for JSON 
+Route::get('/data', function() {
+
+	return 'Get the contents of the JSON object';
 });
---
+++ app/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('_master')
+
+@section('content')
+	<h1>Welcome to Developers Best Friend!</h1>
+	Search: <input type="text">
+@stop
